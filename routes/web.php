70f1523662197820adff5baa7af86c38454b0fce
Abdelhalim Drioueche <abdelhalimdrioueche@Abdelhalims-MacBook-Air-2.local>
2017-01-31 16:12:56 -0500

    add route to get all trips

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,13 @@
 
 Route::get('/airports', 'AirportController@getAll');
 
+Route::get('/trips', 'TripController@getAll');
+
 Route::get('/trips/{tripId}', 'TripController@find')->where('tripId', '[a-z0-9]+');
 
 Route::put('/trips/{tripId}/flights/{departCode}-{destCode}', 'TripController@addFlight')
 	->where(['tripId' => '[a-z0-9]+', 'departCode' => '[A-Z]{3}', 'destCode' => '[A-Z]{3}']);
-	
+
 Route::delete('/trips/{tripId}/flights/{departCode}-{destCode}', 'TripController@deleteFlight')
 	->where(['tripId' => '[a-z0-9]+', 'departCode' => '[A-Z]{3}', 'destCode' => '[A-Z]{3}']);
 
